47bfd1e5604bb52d9a66951b3405ff924f67b438
training <training@RubyRailsVM-16.courses.ebi.ac.uk>
2017-10-24 12:07:17 +0100

    Add seed.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20171024103921) do
     t.string "title"
     t.text "description"
     t.string "image_url"
-    t.decimal "price"
+    t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
