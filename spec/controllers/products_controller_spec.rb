c557f14ebd388e7e4ef54186615e62bdb98d1980
training <ygou@ebi.ac.uk>
2017-10-24 16:09:09 +0100

    Add controller test cases

---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe ProductsController, type: :controller do
+  render_views
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #index content" do
+    before do
+      Product.create!(title: 'Product ----------1',
+                      description: 'A great product',
+                      image_url: 'http://somewhere.com/image.png',
+                      price: 123.45)
+    end
+    it "returns http success" do
+      get :index
+      puts response.body
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
