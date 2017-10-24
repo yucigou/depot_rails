84942e8236ed2c0f39d71b8353b67d56d32bea47
training <ygou@ebi.ac.uk>
2017-10-24 15:25:25 +0100

    Add unit tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -25,4 +25,60 @@ RSpec.describe Product, type: :model do
     end
   end
 
+  context "zero price" do
+    before do
+      product.price = 0
+    end
+    it "is invalid" do
+      expect(product.valid?).to be false
+    end
+  end
+
+  context "small price" do
+    before do
+      product.price = 0.0001
+    end
+    it "is invalid" do
+      expect(product.valid?).to be false
+    end
+  end
+
+  context "Presence of Title and Description" do
+    it "is valid" do
+      expect(product.title.blank?).to be false
+    end
+
+    it "is valid" do
+      expect(product.description.blank?).to be false
+    end
+  end
+
+  context "Format of Image URL" do
+    it "is invalid" do
+      expect(%r{\.(gif|jpg|png)\Z}i.match? product.image_url).to be true
+    end
+  end
+
+  context "Unique titles" do
+    before do
+      Product.create(title: 'Product 1',
+                  description: 'A great product',
+                  image_url: 'http://somewhere.com/image.png',
+                  price: 123.45)
+    end
+    it "is invalid" do
+      expect(Product.count).to be 1
+      expect(product.save).to be false
+    end
+  end
+
+  context "Save to DB" do
+    before do
+      product.save!
+    end
+    it "is valid" do
+      expect(Product.count).to be 1
+    end
+  end
+
 end
